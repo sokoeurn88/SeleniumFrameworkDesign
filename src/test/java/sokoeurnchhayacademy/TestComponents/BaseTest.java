26b7c5d9c0cb9772d66a10b0cf88f6804b864d93
sokoeurn88 <sokoeurn.chhaya@gmail.com>
2024-08-21 11:01:42 -0500

    TakesScreenshot

--- src/test/java/sokoeurnchhayacademy/TestComponents/BaseTest.java
+++ src/test/java/sokoeurnchhayacademy/TestComponents/BaseTest.java
@@ -1,58 +1,84 @@
 package sokoeurnchhayacademy.TestComponents;
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.time.Duration;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Properties;
 
+import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import io.github.bonigarcia.wdm.WebDriverManager;
 import sokoeurnchhayacademy.pageobjects.LandingPage;
 
 public class BaseTest {
-	
+
 	public WebDriver driver;
 	public LandingPage landingPage;
 
 	public WebDriver initializeDriver() throws IOException {
-		
-		//properties class
+
+		// properties class
 		Properties prop = new Properties();
-		
-		FileInputStream fis = new FileInputStream(System.getProperty("user.dir")+"\\src\\main\\java\\sokoeurnchhayacademy\\resources\\GlobalData.properties");
-		
+
+		FileInputStream fis = new FileInputStream(System.getProperty("user.dir")
+				+ "\\src\\main\\java\\sokoeurnchhayacademy\\resources\\GlobalData.properties");
+
 		prop.load(fis);
 		String browserName = prop.getProperty("browser");
-		
-		if(browserName.equalsIgnoreCase("chrome")) {
+
+		if (browserName.equalsIgnoreCase("chrome")) {
 			WebDriverManager.chromedriver().setup();
 			driver = new ChromeDriver();
-			
-		} else if(browserName.equalsIgnoreCase("firefox")) {
-			//firefox
-		} else if(browserName.equalsIgnoreCase("edge")) {
-			//edge browser
+
+		} else if (browserName.equalsIgnoreCase("firefox")) {
+			// firefox
+		} else if (browserName.equalsIgnoreCase("edge")) {
+			// edge browser
 		}
-		
-		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));	//common for all browser
-		
+
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10)); // common for all browser
+
 		return driver;
 	}
-	
-	@BeforeMethod(alwaysRun=true)
+
+	public List<HashMap<String, String>> getJsonDataToMap(String filePath) throws IOException {
+
+		// Read Json to String
+		String jsonContent = FileUtils.readFileToString(new File(filePath),
+				StandardCharsets.UTF_8);
+
+		// Convert String to HashMap using Jackson DataBind dependency
+		ObjectMapper mapper = new ObjectMapper();
+
+		List<HashMap<String, String>> data = mapper.readValue(jsonContent,
+				new TypeReference<List<HashMap<String, String>>>() {
+
+				});
+
+		return data;
+	}
+
+	@BeforeMethod(alwaysRun = true)
 	public LandingPage launchApplication() throws IOException {
 		driver = initializeDriver();
 		landingPage = new LandingPage(driver);
 		landingPage.goTo();
-		
+
 		return landingPage;
 	}
-	
-	@AfterMethod(alwaysRun=true)
+
+	@AfterMethod(alwaysRun = true)
 	public void tearDown() throws InterruptedException {
 		Thread.sleep(Duration.ofSeconds(3));
 		driver.quit();
--- src/test/java/sokoeurnchhayacademy/test/SubmitOrderTest.java
+++ src/test/java/sokoeurnchhayacademy/test/SubmitOrderTest.java
@@ -1,13 +1,19 @@
 package sokoeurnchhayacademy.test;
 
+import java.io.File;
 import java.io.IOException;
 import java.time.Duration;
+import java.util.HashMap;
 import java.util.List;
 
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
@@ -22,17 +28,17 @@ import sokoeurnchhayacademy.pageobjects.ProductCatalogue;
 public class SubmitOrderTest extends BaseTest {
 	String productName = "ZARA COAT 3";
 	
-	@Test
-	public void submitOrder() throws IOException, InterruptedException{
-		ProductCatalogue productCatalogue = landingPage.loginApplication("xyzstars@gmail.com", "Xyz12345");
+	@Test(dataProvider="getData", groups="Purchase")
+	public void submitOrder(HashMap<String,String> input) throws IOException, InterruptedException{
+		ProductCatalogue productCatalogue = landingPage.loginApplication(input.get("email"), input.get("password"));
 		
 //		ProductCatalogue productCatalogue = new ProductCatalogue(driver);
 		List<WebElement> products = productCatalogue.getProductList();
-		productCatalogue.addProductToCart(productName);
+		productCatalogue.addProductToCart(input.get("product"));
 		CartPage cartPage = productCatalogue.goToCartPage();
 		
 //		CartPage cartPage = new CartPage(driver);
-		Boolean match = cartPage.VerifyProductDisplay(productName);
+		Boolean match = cartPage.VerifyProductDisplay(input.get("product"));
 		
 		Assert.assertTrue(match);
 		
@@ -51,7 +57,49 @@ public class SubmitOrderTest extends BaseTest {
 	public void orderHistoryTest() {
 		ProductCatalogue productCatalogue = landingPage.loginApplication("xyzstars@gmail.com", "Xyz12345");
 		OrderPage ordersPage = productCatalogue.goToOrderPage();
-		Assert.assertTrue(ordersPage.VerifyOrderDisplay(productName));
+		ordersPage.VerifyOrderDisplay(productName);
+	}
+	
+	public String getScreenshot(String testCaseName) throws IOException {
+		
+		TakesScreenshot ts = (TakesScreenshot) driver;
+		File source = ts.getScreenshotAs(OutputType.FILE);
+		File file = new File(System.getProperty("user.dir")+"//reports//"+ testCaseName +".png");
+		
+		//package
+		FileUtils.copyFile(source, file);
+		
+		return System.getProperty("user.dir")+"//reports//"+ testCaseName +".png";
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	@DataProvider
+	public Object[][] getData() throws IOException {
+		
+//		HashMap<String,String> map = new HashMap<String,String>();
+//		map.put("email", "xyzstars@gmail.com");
+//		map.put("password", "Xyz12345");
+//		map.put("product", "ZARA COAT 3");
+//		
+//		HashMap<String,String> map1 = new HashMap<String,String>();
+//		map1.put("email", "xyzstars@gmail.com");
+//		map1.put("password", "Xyz12345");
+//		map1.put("product", "ADIDAS ORIGINAL");
+		
+		List<HashMap<String,String>> data = getJsonDataToMap(System.getProperty("user.dir")+"\\src\\test\\java\\sokoeurnchhayacademy\\data\\PurchaseOrder.json");
+		
+		
+		return new Object[][] {{data.get(0)},{data.get(1)}};
+		
 	}
 
 }
